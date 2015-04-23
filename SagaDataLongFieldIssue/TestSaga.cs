0d548778f7f7e24e66bd1a4ad1f0e0a3d4f7e3d0
pablocastilla <pablo.castilla@gmail.com>
2015-04-23 12:59:07 +0200

    first commit

---
+++ SagaDataLongFieldIssue/TestSaga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus.Saga;
+
+namespace SagaDataLongFieldIssue
+{
+    public class TestSaga : Saga<TestSagaData>, IAmStartedByMessages<TestCommand>
+    {
+        public void Handle(TestCommand message)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestSagaData> mapper)
+        {
+           
+        }
+    }
+}
---
+++ SagaDataLongFieldIssue/TestSagaData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus.Saga;
+
+namespace SagaDataLongFieldIssue
+{
+    public class TestSagaData : ContainSagaData
+    {
+        //if DisconnectTransactionInProgress is changed to DisconnectTranInProgress the table for the saga is created
+        public virtual string DisconnectTransactionInProgress { get; set; }
+    }
+}
